69f4b30b834df7689d4201e3cff627fd2f99783c
Shruti Waghmode <waghmodeshru@gmail.com>
2022-04-25 16:49:48 +0530

    update

--- src/components/EmployeeDetails.jsx
+++ src/components/EmployeeDetails.jsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 export const EmployeeDetails = () => {
+  const [employee, setEmployee] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/employee/${id}`)
+      .then((res) => setEmployee(res.data));
+  }, []);
+
+  // var number = Math.floor(Math.random() *(5000))
+  console.log(employee);
   return (
     <div className="user_details">
       <img className="user_image" />
@@ -12,7 +27,8 @@ export const EmployeeDetails = () => {
       {/* Show this button only if user is not already terminated (users status is working) */}
       <button className="fire">Fire Employee</button>
       {/* Show this button only if user is not already team lead or terminated */}
-      <button className="promote">promote</button>
+<button className="promote">promote</button>
     </div>
   );
 };
+
